dc1aaf9f77840254197ffb365a24e74b336349d1
Haniharmaini <haniharmaini2017@gmail.com>
2024-04-26 20:15:17 +0700

    update index.php

--- index.php
+++ index.php
@@ -1,6 +1,5 @@
 <!doctype html>
 <html lang="en">
-
 <head>
   <meta charset="utf-8">
   <meta name="viewport" content="width=device-width, initial-scale=1">
@@ -17,7 +16,6 @@
           <h3 class="text-center">Login System</h3>
           <hr>
           <div class="input-group mb-3">
-            
             <span class="input-group-text">
               <i class="bi bi-person-fill"></i>
             </span>
@@ -41,5 +39,4 @@
   </div>
   <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY31HB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIds1K1eN7N6jIeHz" crossorigin="anonymous"></script>
 </body>
-
 </html>
