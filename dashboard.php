eaa14b7ccaf381be0774c514f5d44a657f709097
Haniharmaini <haniharmaini2017@gmail.com>
2024-05-29 19:44:32 +0700

    update

--- dashboard.php
+++ dashboard.php
@@ -1,3 +1,12 @@
+<?php
+session_start();
+if (!isset($_SESSION['username'])) {
+    header('location: index.php');
+}else{
+    include_once('koneksi.php');
+
+?>
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -35,7 +44,7 @@
                         </button>
                         <ul class="dropdown-menu">
                             <li>
-                                <a class="dropdown-item" href="?modul-profile">
+                                <a class="dropdown-item" href="?modul=profile">
                                     <i class="bi bi-person-badge-fill"></i>
                                     Profile
                                 </a>
@@ -61,3 +70,6 @@
     <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
 </body>
 </html>
+<?php
+}
+?>
