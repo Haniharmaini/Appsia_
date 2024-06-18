96acac5cc9b88aa8415df32b246dbe2ab8c591db
DewiAgustina04 <Dewiyaagustina@gmail.com>
2024-06-18 23:35:17 +0700

    modified file authentication

--- authentication.php
+++ authentication.php
@@ -1,33 +1,36 @@
 <?php
-    // Mulai session
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    require_once('koneksi.php');
     session_start();
 
-    include_once('koneksi.php');
-
-    // Mendapatkan data dari form login
     $username = $_POST['username'];
-    $plainTextPassword = $_POST['password'];
+    $password = $_POST['password'];
 
-    // Query untuk mencari user di database
-    $result = $conn->query("SELECT * FROM pengguna WHERE username = '$username'");
-
-    // Cek apakah user ditemukan
-    if ($result->num_rows > 0) {
-        $pengguna = $result->fetch_assoc();
-        $hashedPassword = $pengguna['password']; // Ambil hash password dari database
-
-        // Verifikasi password
-        if (password_verify($plainTextPassword, $hashedPassword)) {
-            // Password cocok
-            $_SESSION['username'] = $username;
-            header('Location: dashboard.php');
-        } else {
-            // Password tidak cocok
-            echo "Password salah.";
-        }
-    } else {
-        echo "User tidak ditemukan.";
+    $koneksi = new mysqli("localhost", "root", "", "app_sia");
+    if ($koneksi->connect_error) {
+        die("Koneksi database gagal: " . $koneksi->connect_error);
     }
 
-    $conn->close();
+    $query = "SELECT * FROM pengguna WHERE username='$username'";
+    $result = $koneksi->query($query);
+    $row = $result->fetch_assoc();
+    
+    if ($result->num_rows > 0) {
+        if (password_verify($password, $row['password'])) {
+            $_SESSION['username'] = $row['username'];
+            $_SESSION['nama_lengkap'] = $row['nama_lengkap'];
+            $_SESSION['jabatan'] = $row['jabatan'];
+            $_SESSION['hak_akses'] = $row['hak_akses'];
+            header('location: dashboard.php');
+        } else {
+            $_SESSION['pesan'] = "Username atau password tidak valid!!!";
+            header('location:index.php');
+        }
+    } else {
+        $_SESSION['pesan'] = "Username atau password tidak valid!!!";
+        header('location:index.php');
+    }
+} else {
+    header('location:index.php');
+}
 ?>
